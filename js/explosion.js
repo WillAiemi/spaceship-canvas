7e4d1a25cb8ac1e9fbf2f57ed95fd6784644c463
WillAiemi <willianm.cduarte@gmail.com>
2019-05-02 11:24:22 -0300

    fix explosion bug

--- js/explosion.js
+++ js/explosion.js
@@ -11,23 +11,14 @@ function Explosion(context, image, ovni) {
 
 Explosion.prototype = {
     update: function() {
+        this.x = this.ovni.x + this.ovni.image.width / 2 - this.image.width / 10;
+        this.y = this.ovni.y + this.ovni.image.height / 2 - this.image.height / 2;
         let sheet = this.spritesheet;
 
         switch (sheet.column) {
             // if explosion is in its final sprite, self destroy
             case 4:
                 this.animation.removeSprite(this);
-                break;
-            case 1:
-                this.animation.turnOff();
-                /*
-                console.log("x: " + this.x + "\n"
-                    + "y: " + this.y + "\n"
-                    + "ovni x: " + this.ovni.x + "\n"
-                    + "ovni y: " + this.ovni.y + "\n"
-                    + "supposed to be x: " + (this.ovni.x + this.ovni.image.width / 2 - this.image.width / 10) + "\n"
-                    + "supposed to b y: " + (this.ovni.y + this.ovni.image.height / 2 - this.image.height / 2) + "\n");
-                */
                 break;
             // if explosion is in the third frame: destroy the ovni
             case 2:
